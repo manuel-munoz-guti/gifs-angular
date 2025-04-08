d7c74b3b446e0a4caac36d3a9c6536436f44b00a
Jose Manuel Munoz Gutierrez <manuelinux@gmail.com>
2025-04-07 23:15:13 -0400

    second commit

--- src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { GifService } from '../../../services/gifs.service';
 
 interface menuOptions {
   icon: string;
@@ -14,6 +15,7 @@ interface menuOptions {
   templateUrl: './side-menu-options.component.html'
 })
 export class SideMenuOptionsComponent {
+  gifService = inject(GifService);
   menuOptions: menuOptions[] = [
     {
       icon: 'fa-solid fa-chart-line',
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,20 +1,29 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable, signal } from '@angular/core';
+import { computed, effect, inject, Injectable, signal } from '@angular/core';
 import { environment } from '@environments/environment';
 import type { GiphyResponse } from '../interfaces/giphy.interfaces';
 import { Gif } from '../interfaces/gif.interface';
 import { GifMapper } from '../mapper/gif.mapper';
-import { map } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class GifService {
   private http = inject(HttpClient);
   trendingGifs = signal<Gif[]>([]);
   trendingGifsLoading = signal<boolean>(true);
+  searchHistory = signal<Record<string, Gif[]>>({});
+  // cuando queremos tener algo calculado usamos propiedades computadas
+  searchHistoryKey = computed(() => Object.keys(this.searchHistory()));
 
   constructor() {
     this.loadTrendingGifs();
+    this.getHistoryFromLocalStorage();
   }
+
+  // cada vez que se actualiza el search history que es una senial se guarda en local storage
+  saveGifsToLocalStorage = effect(() => {
+    localStorage.setItem('gifs', JSON.stringify(this.searchHistory()));
+  });
 
   loadTrendingGifs() {
     this.http.get<GiphyResponse>(`${ environment.guphyUrl }/gifs/trending`, {
@@ -29,7 +38,7 @@ export class GifService {
     });
   }
 
-  searchGifs(query: string) {
+  searchGifs(query: string): Observable<Gif[]> {
     return this.http.get<GiphyResponse>(`${ environment.guphyUrl }/gifs/search`, {
       params: {
         api_key: environment.giphyApiKey,
@@ -39,9 +48,27 @@ export class GifService {
     }).pipe(
       map( ({ data }) => data),
       map( (items) => GifMapper.mapGiphyItemsToGifArray(items) ),
+      // efecto secundario
+      tap( (items) => {
+        this.searchHistory.update( (history) => ({
+          ...history,
+          [query.toLowerCase()]: items,
+        }));
+      }),
     );
     // si no hay subscribe no se ejecuta el observable por lo tanto no se ejecuta la peticion
     // si queremos trasnformar el observable para que devuelva un array de gifs necesitamos usar un pipe como tap
     // el tap no modifica la respuesta, solo ejecuta un efecto secundario pero map si
+  }
+
+  getHistoryGifs( query: string ): Gif[] {
+    return this.searchHistory()[query.toLowerCase()] ?? [];
+  };
+
+  getHistoryFromLocalStorage() {
+    const history = localStorage.getItem('gifs');
+    if (history) {
+      this.searchHistory.set(JSON.parse(history));
+    }
   }
 }
