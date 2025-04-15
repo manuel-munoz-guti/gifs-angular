fff4b5a8c2ce07b0ea94502d0020ab00cfbd5fe6
Jose Manuel Munoz Gutierrez <manuelinux@gmail.com>
2025-04-14 21:15:58 -0400

    final commit

--- src/app/gifs/pages/trending-page/trending-page.component.ts
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, inject, viewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, viewChild } from '@angular/core';
 import { GifService } from '../../services/gifs.service';
 import { ScrollStateService } from '../../../shared/services/scroll-state.service';
 
@@ -7,15 +7,22 @@ import { ScrollStateService } from '../../../shared/services/scroll-state.servic
   imports: [],
   templateUrl: './trending-page.component.html',
 })
-export default class TrendingPageComponent {
+export default class TrendingPageComponent implements AfterViewInit{
 
   gifService = inject(GifService);
   scrollStateService = inject(ScrollStateService);
   scrollDivRef = viewChild<ElementRef<HTMLDivElement>>('groupDiv');
 
+  // Esto se ejecutara tan pronto la vista se inicializa
+  ngAfterViewInit(): void {
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+    if (!scrollDiv) return;
+
+    scrollDiv.scrollTop = this.scrollStateService.trendingScrollState();
+  }
+
   onScroll(event: Event) {
     const scrollDiv = this.scrollDivRef()?.nativeElement;
-
     if (!scrollDiv) return;
 
     const scrollTop = scrollDiv.scrollTop;
